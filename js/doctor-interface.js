dc5f8dcdfddaff76b2efd7e29ff5a4908422b0c8
Keegan Ruebling <keegan.ruebling@gmail.com>
2017-10-20 13:50:15 -0700

    apply google map api from two day project

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -2,29 +2,40 @@ import { Doctor } from './../js/doctor.js';
 
 $(document).ready(function() {
   $('.input').submit(function(event) {
-    $('#results').append("<img src = 'img/loading.gif'>");
     event.preventDefault();
 
-    let name = $('#name').val();
-    let query = $('#query').val();
+    let location = $('#location').val();
     let doctor = new Doctor();
-    let searchQuery = doctor.search(name, query);
+    let locQuery = doctor.locQuery(location);
 
-    searchQuery.then(function(response) {
+    locQuery.then(function(response) {
       let body = JSON.parse(response);
+      let lat = body.results[0].geometry.location.lat;
+      let lng = body.results[0].geometry.location.lng;
+      debugger;
+      let name = $('#name').val();
+      let query = $('#query').val();
+      let docQuery = doctor.docQuery(lat, lng, name, query);
+      // $('#results').append("<img src = 'img/loading.gif'>");
 
-      $('#results').text('');
-      if (body.data.length > 0) {
-      body.data.forEach(function(data) {
-        data.practices.forEach(function(practice) {
-        $('#results').append(`<tr><td>${practice.name}</td><td>${practice.visit_address.street}</td><td>${practice.visit_address.city}</td><td>${practice.phones[0].number}</td><td>${practice.website}</td><td>${practice.accepts_new_patients}</td></tr>`);
-      });
-    });
-    } else {
-      $('#results').text("No Doctors Found");
-    }
-    }, function(error) {
-    $('#showErrors').text(`There was an error processing your request: ${error.message}`);
+        // Doctor Search
+        docQuery.then(function(response) {
+          let body = JSON.parse(response);
+          $('#results').text('');
+          if (body.data.length > 0) {
+            body.data.forEach(function(data) {
+              data.practices.forEach(function(practice) {
+                $('#results').append(`<tr><td>${practice.name}</td><td>${practice.visit_address.street}</td><td>${practice.visit_address.city}</td><td>${practice.phones[0].number}</td><td>${practice.website}</td><td>${practice.accepts_new_patients}</td></tr>`);
+              });
+            });
+          } else {
+            $('#results').text("No Doctors Found");
+          }
+        }, function(error) {
+          $('#showErrors').text(`There was an error processing your doctor query: ${error.message}`);
+        });
+      }, function(error) {
+        $('#showErrors').text(`There was an error processing your location query: ${error.message}`);
     });
   });
 });
--- js/doctor.js
+++ js/doctor.js
@@ -2,10 +2,26 @@ var apiKey = require('./../.env').apiKey;
 
 export class Doctor {
 
-  search(name, query) {
+  locQuery(location) {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&query=${query}&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${apiKey}`;
+      let url = `https://maps.googleapis.com/maps/api/geocode/json?address=${location}&key=AIzaSyBb-6lyykgnZhSEv_FdW6BWi_7BjznhOmw`;
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      };
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
+
+  docQuery(lat, lng, name, query) {
+    return new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&query=${query}&user_location=${lat}%2C${lng}&skip=0&limit=25&user_key=${apiKey}`;
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
