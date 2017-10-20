35c6bd4288fdd12527280aaf746c52a197ed686d
Keegan Ruebling <keegan.ruebling@gmail.com>
2017-10-20 10:54:00 -0700

    reformat search for both doctor name and practice

--- js/doctor.js
+++ js/doctor.js
@@ -1,1 +1,21 @@
 var apiKey = require('./../.env').apiKey;
+
+export class Doctor {
+
+  search(name, query) {
+    return new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&query=${query}&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${apiKey}`;
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      };
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
+
+}
