767e900228eb8181fdf7d02be4f2df4cd25fb3fc
Keegan Ruebling <keegan.ruebling@gmail.com>
2017-10-20 12:31:19 -0700

    create successful search query, displaying doctor name and address

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -6,21 +6,19 @@ $(document).ready(function() {
     event.preventDefault();
 
     let name = $('#name').val();
-    let query = $('#query').val();
-
-    let user = new Doctor();
-    let searchQuery = user.search(name, query);
+    let doctor = new Doctor();
+    let searchQuery = doctor.search(name);
 
     searchQuery.then(function(response) {
       let body = JSON.parse(response);
-      $('#doctor').append("<img src = 'img/loading.gif'>");
-      debugger;
-
-      $('#doctor').text('');
-      if (body > 0) {
-      body.profile.forEach(function(doctor) {
-        $('#doctor').append(`<tr><td>${doctor.properties.place}</td><td>${new Date(doctor.properties.time)}</td><td>${doctor.properties.mag}</td><td>${doctor.properties.tsunami}</td><td style = 'background-color:${color};'>${doctor.properties.alert}</td><td>${doctor.properties.felt}</td></tr> `);
+      $('#results').append("<img src = 'img/loading.gif'>");
+      $('#results').text('');
+      if (body.data.length > 0) {
+      body.data.forEach(function(data) {
+        data.practices.forEach(function(practice) {
+        $('#results').append(`<tr><td>${practice.name}</td><td>${practice.visit_address.street}</td><td>${practice.phones.number}</td></td>`);
       });
+    });
     } else {
       $('#doctor').text("No Doctors Found'");
     }
